7354c874e1d8b845566b64e67da6edd92f56e198
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-06-06 17:46:34 +0700

    added multer libs and generate filename code

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const uploadDestination = 'uploads';
 
-const storage = multer.diskStorage();
+const storage = multer.diskStorage({
+    destination: uploadDestination,
+    filename: function(req, file, next)  {
+        next(null, file.originalname);
+    }
+});
+
+const uploads = multer({storage: storage});
 
 router.get('/register', (req, res) => {
     res.send('register');
