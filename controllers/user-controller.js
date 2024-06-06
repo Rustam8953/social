cb1a64501e61a9b6ae14fbbb25f080dd835996c5
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-06-06 22:36:06 +0700

    added login users function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -38,7 +38,25 @@ const UserController = {
         }
     },
     login: async (req, res) => {
-
+        const {email, password} = req.body;
+        if(!email || !password) {
+            return res.status(400).json({error: "Не все поля заполнены!"});
+        }
+        try {
+            const user = await prisma.user.findUnique({where: {email}});
+            if(!user) {
+                return res.status(400).json({error: "Не верный логин или пароль"});
+            }
+            const valid = await bcrypt.compare(password, user.password);
+            if(!valid) {
+                return res.status(400).json({error: "Неверно введены данные!"})
+            }
+            const token = jwt.sign(({userId: user.id}), process.env.SECRET_KEY);
+            res.json({token});
+        } catch (error) {
+            console.error("Sign Error", error);
+            res.status(500).json({error: "Ошибка входа"})
+        }
     },
     getUserById: async (req, res) => {
         res.send('getUser')
