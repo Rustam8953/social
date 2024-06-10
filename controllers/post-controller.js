68f8c7c490f7be5535ead021933d0eeba8b87632
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-06-10 23:44:22 +0700

    function create post

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -1,7 +1,22 @@
 const {prisma} = require("../prisma/prisma-client");
 const PostController = {
     createPost: async (req, res) => {
-        res.send('create post');
+        const {content} = req.body;
+        const authorId = req.user.userId;
+        if(!content) return res.status(400).json({error: "Все поля обязательны!"});
+
+        try {
+            const post = await prisma.post.create({
+                data: {
+                    content,
+                    authorId
+                }
+            })
+            res.json(post);
+        } catch (error) {
+            console.error("Created post error", error);
+            res.status(500).json({error: "Internal server error", error});
+        }
     },
     updatePost: async (req, res) => {
         res.send("Update post");
